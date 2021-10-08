cf5c280e72be9b79a04f0c9fb72015022818fa1e
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2021-10-08 18:04:03 +1100

    binary search update

--- specs/search/search.test.js
+++ specs/search/search.test.js
@@ -8,7 +8,20 @@ function linearSearch(id, array) {
 }
 
 function binarySearch(id, array) {
-  // code goes here
+  let middle = 0;
+  let start = 0;
+  let end = array.length - 1;
+
+  for (let i = 0; i < array.length; i++) {
+    middle = Math.floor((end - start)/2);
+    if (array[i].id == id) {
+      return array[i];
+    } else if (array[middle].id > id){
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
 }
 
 // unit tests
@@ -35,7 +48,7 @@ test.skip("linear search", function () {
   ).toBe(lookingFor);
 });
 
-test.skip("binary search", function () {
+test("binary search", function () {
   const lookingFor = { id: 23, name: "Brian" };
   expect(
     binarySearch(23, [
